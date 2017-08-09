9f5554bde6367d26ea5f767d0d0ffd347f505c50
Craig Quincy <craig.quincy@galvanize.com>
2017-08-09 07:27:46 -0600

    add lint file

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+ "extends": [
+    'airbnb'
+  ],
+  rules: {
+    "semi": "off",
+    "prefer-const": "off",
+    "no-implicit-globals": "off",
+    "strict": "off",
+    "quotes": "off",
+    "no-unused-vars": "off",
+    "no-else-return": "off",
+    "no-multiple-empty-lines": "off",
+    "no-shadow" : "off",
+    "comma-dangle" : "off",
+    "func-names" : "off",
+    "no-undef" : "off",
+    "no-restricted-syntax" : "off"
+  }
+};
+
